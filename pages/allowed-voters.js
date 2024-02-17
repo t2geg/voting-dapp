20e5b30799182101813697b9edf91e4af4ca5217
Ashish Kumar Gupta <87427147+t2geg@users.noreply.github.com>
2024-02-17 23:23:28 +0530

    fixed small error in voter form

--- pages/allowed-voters.js
+++ pages/allowed-voters.js
@@ -66,28 +66,10 @@ const allowedVoters = () => {
         {!fileUrl && (
           <div className={Style.sideInfo}>
             <div className={Style.sideInfo_box}>
-              <h4>Create Candidate For Voting</h4>
+              <h4>Create Voter For Voting</h4>
               <p>
                 Blockchain voting Organisation, providing ETHEREUM blockchain ecosystem
               </p>
-              <p className={Style.sideInfo_para}>Contract Candidate List</p>
-            </div>
-            <div className={Style.car}>
-              {voterArray.map((el, i) => (
-                <div key={i + 1} className={Style.card_box}>
-                  <div className={Style.image}>
-                    <img src={el[4]} alt="Profile photo" />
-                  </div>
-
-                  <div className={Style.card_info}>
-                    <p>
-                      {el[1]} #{el[1].toNumber()}
-                    </p>
-                    <p>{el[0]}</p>
-                    <p>Address: {el[3].slice(0, 10)}..</p>
-                  </div>
-                </div>
-              ))}
             </div>
           </div>
         )}
