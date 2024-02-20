6449d54073ea0de7f3f35752a0c294deb06adae7
Ashish Kumar Gupta <87427147+t2geg@users.noreply.github.com>
2024-02-20 23:13:26 +0530

    update: next.js 12->next.js 13

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 import upload from "./upload.png";
 import creator from "./notice.png";
 import logo from "./logo.png";
+import home_image from './voting_home.png'
 
-export default { upload, creator, logo };
+export default { upload, creator, logo, home_image };
