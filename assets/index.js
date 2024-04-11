a9a5aac4cb8e24b8ae5b02dbe29acb3e8cb6b413
Ashish Kumar Gupta <87427147+t2geg@users.noreply.github.com>
2024-04-11 20:46:11 +0530

    update: dependencies & homepage

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 import upload from "./upload.png";
 import creator from "./notice.png";
 import logo from "./logo.png";
-import home_image from './voting_home.png'
+import home_image from './votinghome.jpeg'
 
 export default { upload, creator, logo, home_image };
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect, useContext } from "react";
 import Countdown from "react-countdown";
 import images from "../assets";
 import Image from "next/image";
+import { Typography, Button } from '@mui/material';
+import Head from 'next/head'
 
 //INTERNAL IMPORT
 import { VotingContext } from "../context/Voter";
 import Style from "../styles/index.module.css";
 import Card from "../components/card/card";
+import { CustomButton } from "../components/Button/Button.jsx"
+
 
 const index = () => {
   const {
@@ -25,32 +29,70 @@ const index = () => {
   }, []);
 
   return (
-    <div className={Style.home}>
-      {currentAccount && (
-        <div className={Style.winner}>
-          <div className={Style.winner_info}>
-            <div className={Style.candidate_list}>
-              <p>
-                No. of Candidates:<span>{candidateLength}</span>
-              </p>
-            </div>
-            <div className={Style.candidate_list}>
-              <p>
-                No. of Voters:<span>{voterLength}</span>
-              </p>
-            </div>
-          </div>
-          <div className={Style.winner_message}>
-            <small>
-              <Countdown date={Date.now() + 1000000000} />
-            </small>
-          </div>
-        </div>
-      )
-      }
+    <>
+      <div>
+        <Head>
+          <title>
+            Decentralised E-Voting
+          </title>
+        </Head>
+      </div>
+      <div className={Style.homepage1}>
+        <div className={Style.text1}>
+          <Typography variant="h3" align="center" fontWeight={"Bold"}>
+            Vote as per convenience with our
+          </Typography>
 
-      <Card candidateArray={candidateArray} giveVote={giveVote} />
-    </div >
+          <Typography variant="h3" align="center" color={"#0096FF"} fontWeight={"Bold"}>
+            Decentralised Voting App
+          </Typography> <br></br>
+          <Typography variant="subtitle1" align="center" style={{
+            marginTop: 20,
+            marginBottom: 30,
+          }}>
+            Our decentralized voting app puts the power of democracy in your hands.
+            Say goodbye to concerns about fraud and manipulation—our blockchain-based platform ensures the integrity of every vote. Easy to use and highly secure, our app enables you to participate in elections from anywhere. Join us in shaping the future of governance.
+          </Typography>
+          <Button variant="contained" color="primary" style={{
+            margin: " 0 auto",
+            display: "flex",
+          }}>
+            Get Started
+          </Button>
+        </div>
+
+
+      </div >
+
+      <div className={Style.home}>
+        {currentAccount && (
+          <div className={Style.winner}>
+            <div className={Style.winner_info}>
+              <div className={Style.candidate_list}>
+                <p>
+                  No. of Candidates:<span>{candidateLength}</span>
+                </p>
+              </div>
+              <div className={Style.candidate_list}>
+                <p>
+                  No. of Voters:<span>{voterLength}</span>
+                </p>
+              </div>
+            </div>
+            <div className={Style.winner_message}>
+              <small>
+                <Countdown date={Date.now() + 1000000000} />
+              </small>
+            </div>
+          </div>
+        )
+        }
+
+        <Card candidateArray={candidateArray} giveVote={giveVote} />
+      </div >
+
+
+    </>
   );
 };
 
